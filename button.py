2f9059aef98967baf3d11a1e033adff3733eff4b
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2022-01-12 17:10:02 -0500

    Update button.py

--- button.py
+++ button.py
@@ -2,7 +2,7 @@ import pygame
 
 #button class
 class Button():
-	def __init__(self,x, y, image, scale):
+	def __init__(self, x: int, y: int, image: str, scale: int) -> None:
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -10,7 +10,7 @@ class Button():
 		self.rect.topleft = (x, y)
 		self.clicked = False
 
-	def draw(self, surface):
+	def draw(self, surface: str) -> bool:
 		action = False
 
 		#get mouse position
